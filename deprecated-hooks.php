ba3554fa82fcc2771a0db8d364de17c2ac4c8444
Evan Mattson <me@aaemnnost.tv>
2015-09-30 22:16:35 -0400

    indentation to spaces

--- deprecated-hooks.php
+++ deprecated-hooks.php
@@ -3,170 +3,170 @@
 
 class Hook
 {
-	const DEPRECATED_HANDLER = '';
-	protected static $deprecated_tags = [ ];
+    const DEPRECATED_HANDLER = '';
+    protected static $deprecated_tags = [ ];
 
-	public static function deprecate( $tag )
-	{
-		if ( empty( self::$deprecated_tags[ $tag ] ) ) {
-			$handler = __NAMESPACE__ . '\\' . static::DEPRECATED_HANDLER;
-			self::$deprecated_tags[ $tag ] = new $handler( $tag );
-		}
+    public static function deprecate( $tag )
+    {
+        if ( empty( self::$deprecated_tags[ $tag ] ) ) {
+            $handler = __NAMESPACE__ . '\\' . static::DEPRECATED_HANDLER;
+            self::$deprecated_tags[ $tag ] = new $handler( $tag );
+        }
 
-		return self::$deprecated_tags[ $tag ];
-	}
+        return self::$deprecated_tags[ $tag ];
+    }
 }
 
 class Filter extends Hook
 {
-	const DEPRECATED_HANDLER = 'Deprecated_Filter';
+    const DEPRECATED_HANDLER = 'Deprecated_Filter';
 }
 
 class Action extends Hook
 {
-	const DEPRECATED_HANDLER = 'Deprecated_Action';
+    const DEPRECATED_HANDLER = 'Deprecated_Action';
 }
 
 
 class Deprecated_Filter
 {
-	const TYPE = 'filter';
-	const FUNC = 'add_filter';
+    const TYPE = 'filter';
+    const FUNC = 'add_filter';
 
-	/**
-	 * The deprecated tag
-	 * @var string
-	 */
-	protected $tag;
+    /**
+     * The deprecated tag
+     * @var string
+     */
+    protected $tag;
 
-	/**
-	 * The tag to use instead
-	 * @var string
-	 */
-	protected $alternative;
+    /**
+     * The tag to use instead
+     * @var string
+     */
+    protected $alternative;
 
-	/**
-	 * The version the tag was deprecated
-	 * @var string
-	 */
-	protected $since;
+    /**
+     * The version the tag was deprecated
+     * @var string
+     */
+    protected $since;
 
-	public function __construct( $tag )
-	{
-		$this->tag = $tag;
+    public function __construct( $tag )
+    {
+        $this->tag = $tag;
 
-		$this->set_listener();
-	}
+        $this->set_listener();
+    }
 
-	/**
-	 * Sets the alternative tag which should be used instead
-	 *
-	 * @param $tag
-	 *
-	 * @return $this
-	 */
-	public function instead( $tag )
-	{
-		$this->alternative = $tag;
+    /**
+     * Sets the alternative tag which should be used instead
+     *
+     * @param $tag
+     *
+     * @return $this
+     */
+    public function instead( $tag )
+    {
+        $this->alternative = $tag;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * Sets the version the deprecation started
-	 *
-	 * @param $version
-	 *
-	 * @return $this
-	 */
-	public function since( $version )
-	{
-		$this->since = $version;
+    /**
+     * Sets the version the deprecation started
+     *
+     * @param $version
+     *
+     * @return $this
+     */
+    public function since( $version )
+    {
+        $this->since = $version;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * Get the message which is used to inform the user if the tag is used
-	 *
-	 * @return string
-	 */
-	public function get_message()
-	{
-		$message = "The ". static::TYPE ." tag '$this->tag' is deprecated.";
+    /**
+     * Get the message which is used to inform the user if the tag is used
+     *
+     * @return string
+     */
+    public function get_message()
+    {
+        $message = "The ". static::TYPE ." tag '$this->tag' is deprecated.";
 
-		if ( $this->alternative ) {
-			$message .= " Use '$this->alternative' instead.";
-		}
+        if ( $this->alternative ) {
+            $message .= " Use '$this->alternative' instead.";
+        }
 
-		return $message;
-	}
+        return $message;
+    }
 
-	/**
-	 * Setup a callback to listen for usage
-	 */
-	protected function set_listener()
-	{
-		// add_action is a wrapper for add_filter
-		add_filter($this->tag, [$this, 'listener_callback'], 0);
-	}
+    /**
+     * Setup a callback to listen for usage
+     */
+    protected function set_listener()
+    {
+        // add_action is a wrapper for add_filter
+        add_filter($this->tag, [$this, 'listener_callback'], 0);
+    }
 
-	/**
-	 * The deprecated tag has been used
-	 * @return mixed
-	 */
-	public function listener_callback()
-	{
-		// remove this callback to not interfere with our check
-		remove_filter($this->tag, [$this, 'listener_callback'], 0);
+    /**
+     * The deprecated tag has been used
+     * @return mixed
+     */
+    public function listener_callback()
+    {
+        // remove this callback to not interfere with our check
+        remove_filter($this->tag, [$this, 'listener_callback'], 0);
 
-		// check for any registered callbacks on this
-		if ( has_filter($this->tag) ) {
-			$this->report_usage();
-		}
+        // check for any registered callbacks on this
+        if ( has_filter($this->tag) ) {
+            $this->report_usage();
+        }
 
-		// reset the listener on a later priority to avoid an infinite loop
-		add_filter($this->tag, [$this, 'reset_listener']);
+        // reset the listener on a later priority to avoid an infinite loop
+        add_filter($this->tag, [$this, 'reset_listener']);
 
-		// return the first argument to not break a filter
-		return $this->get_return( func_get_args() );
-	}
+        // return the first argument to not break a filter
+        return $this->get_return( func_get_args() );
+    }
 
-	/**
-	 * Reset the listener callback to listen on subsequent uses
-	 * @return mixed
-	 */
-	public function reset_listener()
-	{
-		remove_filter($this->tag, [$this, 'reset_listener']);
+    /**
+     * Reset the listener callback to listen on subsequent uses
+     * @return mixed
+     */
+    public function reset_listener()
+    {
+        remove_filter($this->tag, [$this, 'reset_listener']);
 
-		$this->set_listener();
+        $this->set_listener();
 
-		return $this->get_return( func_get_args() );
-	}
+        return $this->get_return( func_get_args() );
+    }
 
-	/**
-	 * The deprecated tag was used, issue a warning
-	 */
-	protected function report_usage()
-	{
-		_deprecated_argument(static::FUNC, $this->since, $this->get_message());
-	}
+    /**
+     * The deprecated tag was used, issue a warning
+     */
+    protected function report_usage()
+    {
+        _deprecated_argument(static::FUNC, $this->since, $this->get_message());
+    }
 
-	/**
-	 * @param $args
-	 *
-	 * @return mixed
-	 */
-	protected function get_return( $args )
-	{
-		return reset( $args );
-	}
+    /**
+     * @param $args
+     *
+     * @return mixed
+     */
+    protected function get_return( $args )
+    {
+        return reset( $args );
+    }
 }
 
 class Deprecated_Action extends Deprecated_Filter
 {
-	const TYPE = 'action';
-	const FUNC = 'add_action';
+    const TYPE = 'action';
+    const FUNC = 'add_action';
 }
 
